a88e7ca6f935a9d884a148230b1c5fd8da4145ae
justin massena <justin@massena.com>
2015-06-27 14:58:27 -0700

    +more idea working now but I wish the more link was positioned at exact bottom of cell but because cell overflows div that will nott work. mabye i can position them relative to div bottom and some percent offset left

--- src/client/app/calendar/calendarMonthView.directive.js
+++ src/client/app/calendar/calendarMonthView.directive.js
@@ -4,9 +4,9 @@
   angular.module('app')
     .directive('calendarMonthView', calendarMonthView);
 
-  calendarMonthView.$inject = ['$timeout', '$modal', 'UtilitySvc', 'CalendarEditSvc'];
+  calendarMonthView.$inject = ['$timeout', '$modal', '$window', 'UtilitySvc', 'CalendarEditSvc'];
 
-  function calendarMonthView($timeout, $modal, UtilitySvc, CalendarEditSvc) {
+  function calendarMonthView($timeout, $modal, $window, UtilitySvc, CalendarEditSvc) {
 
     // renders the calendar name, color, dropdown arrow for setting properties
     // emits events to communicate with calendar controller for setting color or deactivating calendar.
@@ -18,7 +18,137 @@
         calendarList: '='
       },
 
+      link: function (scope, element, attrs) {
+        // trimDayEvents();
+
+        // scope.$watch(function () {
+        //     return $window.innerHeight + 'px';
+        //   },
+        //   function (newVal, oldVal) {
+        //     console.log('window height: ' + newVal);
+        //   });
+
+        $(window).resize(function () {
+          // console.log('JQ window height: ' + window.innerHeight);
+          scope.trimDayEvents(window.innerHeight, true);
+        });
+
+        // $(window).on('resize.monthView', function () {
+        //   // console.log('JQ window height: ' + window.innerHeight);
+        //   scope.trimDayEvents(window.innerHeight);
+        // });
+        //
+        // scope.$on('$destroy', function () {
+        //   $(window).off('resize.monthView');
+        // });
+
+        // function trimDayEvents(viewHeight) {
+        //   viewHeight = viewHeight || $window.innerHeight;
+        //   var containerHeight = 0.8 * viewHeight;
+        //   var weekHeight = 0.2 * containerHeight;
+        //
+        //   // for now this will be static but later can change this based on browser zoom and actual font-size I hope.
+        //   var eventHeight = 21;
+        //   var maxEvents = Math.floor((weekHeight - 20) / eventHeight);
+        //
+        //   // This is working better than I expected! Only issue is with sweet Nexus 5 items
+        //   // innerHeight reads 640 but body height is actually 750?
+        //   console.log('');
+        //   console.log('max events: ' + maxEvents);
+        //   console.log('viewHeight: ' + viewHeight);
+        //   console.log('containerHeight: ' + containerHeight);
+        //   console.log('weekHeight: ' + weekHeight);
+        //   console.log('eventHeight: ' + eventHeight);
+        //
+        // }
+
+      },
+
       controller: function ($scope) {
+
+        $scope.trimDayEvents = trimDayEvents;
+
+        // $scope.trimDayEvents();
+
+        $scope.$watch(function () {
+            return $scope.monthViewEvents.weeks;
+          },
+          function (newVal, oldVal) {
+            if(newVal && oldVal !== newVal) {
+              $scope.trimDayEvents();
+            }
+          });
+
+        function trimDayEvents(viewHeight, doDigest) {
+          viewHeight = viewHeight || $window.innerHeight;
+          var containerHeight = 0.8 * viewHeight;
+          var weekHeight = 0.2 * containerHeight;
+
+          // for now this will be static but later can change this based on browser zoom and actual font-size I hope.
+          var eventHeight = 21;
+          var maxEvents = Math.floor((weekHeight - 20) / eventHeight);
+
+          console.log('');
+          console.log('max events: ' + maxEvents);
+          console.log('viewHeight: ' + viewHeight);
+          console.log('containerHeight: ' + containerHeight);
+          console.log('weekHeight: ' + weekHeight);
+          console.log('eventHeight: ' + eventHeight);
+
+          if(!$scope.monthViewEvents.weeks) {
+            // object might not be initialized from web service call yet.
+            return;
+          }
+
+          if($scope.previousMaxEvents === maxEvents) {
+            return;
+          }
+
+          // build $scope.trimmedMonthViewEvents from monthViewEvents.
+          var tmpMonthView = angular.copy($scope.monthViewEvents);
+
+          tmpMonthView.weeks.forEach(function (week) {
+            if(week.eventRowsCount > maxEvents + 1) {
+              week.eventRowsCount = maxEvents + 1;
+
+              week.eventRowIndexes = [];
+              for(var i = 0; i < week.eventRowsCount; i++) {
+                week.eventRowIndexes.push(i);
+              }
+
+              week.days.forEach(function (day) {
+
+                if(day.events.length > maxEvents + 1) {
+                  var removedEventsCount = 0;
+                  while(day.events.length >= maxEvents) {
+                    var event = day.events.pop();
+                    if(!event.fillerEvent) {
+                      removedEventsCount++;
+                    }
+                  }
+
+                  // now add 'more events' and then filler;
+                  day.events.push({
+                    moreEventsCount: removedEventsCount,
+                    date: day.date
+                  });
+
+                  day.events.push({
+                    fillerEvent: true,
+                    lastEvent: true,
+                    rowSpan: week.eventRowsCount - day.events.length,
+                    date: day.date
+                  });
+                }
+              });
+            }
+          });
+
+          $scope.trimmedMonthViewEvents = tmpMonthView;
+          if(doDigest) {
+            $scope.$apply();
+          }
+        }
 
         $scope.openDetailsDialog = function ($event, calendarEvent) {
 
